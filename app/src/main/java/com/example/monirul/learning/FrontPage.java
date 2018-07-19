b6ab0c77151f6b624eaeb4c5fc029ba10943750e
monirul-islam7774 <monirul.islam7774@gmail.com>
2018-07-19 18:52:51 +0600

    first commit

---
+++ app/src/main/java/com/example/monirul/learning/FrontPage.java
@@ -0,0 +1,59 @@
+package com.example.monirul.learning;
+
+import android.content.Intent;
+import android.os.Debug;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class FrontPage extends AppCompatActivity implements View.OnClickListener{
+
+    Button simpleQuestion;
+    Button toughQuestion;
+    Button otherApp;
+    Button rateApp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front_page);
+
+        simpleQuestion = findViewById(R.id.simple_questions);
+        toughQuestion = findViewById(R.id.tough_questions);
+        otherApp =   findViewById(R.id.other_app);
+        rateApp = (Button)findViewById(R.id.rate_app);
+
+        simpleQuestion.setOnClickListener(this);
+        toughQuestion.setOnClickListener(this);
+        otherApp.setOnClickListener(this);
+        rateApp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId())
+        {
+
+            case R.id.simple_questions:
+                Intent i = new Intent(FrontPage.this,SimpleQuestion.class);
+                startActivity(i);
+                break;
+            case R.id.tough_questions:
+                Intent j = new Intent(FrontPage.this,ToughQuestions.class);
+                startActivity(j);
+                break;
+
+            case R.id.other_app:
+
+                break;
+
+            case R.id.rate_app:
+
+                break;
+
+        }
+    }
+}
